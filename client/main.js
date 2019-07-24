8e90b183f25342099bca32ccae4e8ecdfd9d0e9b
Daniel Schreij <dschreij@gmail.com>
2019-07-24 21:06:18 +0200

    Eslint set up

--- client/main.js
+++ client/main.js
@@ -1,22 +1,13 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import Vue from 'vue'
+import VueMeteorTracker from 'vue-meteor-tracker'
+Vue.use(VueMeteorTracker)
 
-import './main.html';
+import App from './App.vue'
+import './main.html'
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+Meteor.startup(() => {
+  new Vue({
+    el: '#app',
+    ...App,
+  })
+})
